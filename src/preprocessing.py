b6314d731e1c7befe01351807f57869ac9d686df
Nazar Netterström <nazar.netterstrom@gmail.com>
2022-09-23 15:04:30 +0200

    Fixed encoding of CSV

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,16 @@
+import pandas as pd
+import random
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import statistics as s
+import numpy as np
+from numpy import genfromtxt
+from sklearn.model_selection import KFold
+
+import matplotlib.pyplot as plt
+from sklearn.ensemble import GradientBoostingClassifier
+
+# ------------------- IMPORT DATA ----------------
+data = pd.read_csv (r'data/project_train.csv', encoding='utf-8') # Import data
 
 
-def main():
-    pass
