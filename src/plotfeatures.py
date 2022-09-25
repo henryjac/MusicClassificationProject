2a346c5db620976ffda7a074005451317b1c832f
Henry Jacobson <henry_jacobson@outlook.com>
2022-09-25 22:28:47 +0200

    Change svg to png

--- src/plotfeatures.py
+++ src/plotfeatures.py
@@ -11,23 +11,28 @@ def main():
     inputs = np.delete(inputs,energy_err,0)
 
     loudness_err = np.argmin(inputs[:,3])
-    print(loudness_err)
     inputs = np.delete(inputs,loudness_err,0)
 
-    # for i in range(np.shape(inputs)[1]-1):
-    #     for j in range(np.shape(inputs)[1]-1):
-    #         if i == j:
-    #             continue
-    #         feature_plot(inputs, i, j, data.keys()[i], data.keys()[j])
+    for i in range(np.shape(inputs)[1]-1):
+        for j in range(np.shape(inputs)[1]-1):
+            if i == j:
+                continue
+            feature_plot(inputs, i, j, data.keys()[i], data.keys()[j])
 
 def feature_plot(inputs, f1, f2, namef1, namef2):
-    plt.figure()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
     cmap, nmap = clr.from_levels_and_colors([0,0.5,1],['blue','red'])
     plt.scatter(inputs[:,f1],inputs[:,f2], c=inputs[:,-1], cmap=cmap)
+    ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
     plt.xlabel(namef1)
     plt.ylabel(namef2)
 
-    plt.savefig(f'img/{f1}_vs_{f2}')
+    plt.tight_layout()
+    plt.savefig(f'img/{f1}_vs_{f2}.svg')
     plt.close()
 
 if __name__ == '__main__':
