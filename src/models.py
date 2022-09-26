015d666b77f428e8f46385cf5cf70b97fb134697
Henry Jacobson <henry_jacobson@outlook.com>
2022-09-26 15:29:54 +0200

    Combine specific ML models to one function since they were very similar

--- src/models.py
+++ src/models.py
@@ -1,39 +1,19 @@
+import numpy as np
+
 from sklearn.svm import SVC # Supprort Vector classifier
 from sklearn.neighbors import KNeighborsClassifier # KNN
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import RandomForestClassifier
 
-def svm(X_train, X_test, y_train):
-    #SVM
-    model = SVC(C=1,kernel='linear' )
+def ML_model_prediction(X_train, X_test, y_train, ML_model, **kwargs):
+    model = ML_model(**kwargs)
     model.fit(X_train, y_train)
     labels = model.predict(X_test)
     return labels
 
-def knn(X_train, X_test, y_train):
-    # initialize the model with parameter k
-    KNNClassifier = KNeighborsClassifier(n_neighbors=10, weights='distance',algorithm='auto')
-    # train the model with X_train datapoints and y_train data labels
-    KNNClassifier.fit(X_train, y_train)
-    # returns a classification of a X_test datapoints
-    labels_knn = KNNClassifier.predict(X_test)
-    return labels_knn
-
-def lda(X_train, X_test, y_train):
-    model = LinearDiscriminantAnalysis()
-    model.fit(X_train, y_train)
-    labels = model.predict(X_test)
-    return labels
-
-def qda(X_train, X_test, y_train):
-    model = QuadraticDiscriminantAnalysis( reg_param=0.01)
-    model.fit(X_train, y_train)
-    labels = model.predict(X_test)
-    return labels
-
-def rfc(X_train, X_test, y_train):
-    model = RandomForestClassifier(criterion='entropy', n_estimators=200, max_features='sqrt')
-    model.fit(X_train, y_train)
-    labels = model.predict(X_test)
-    return labels
+def get_accuracy(X_train, X_test, y_train, y_test, ML_model, **kwargs):
+    labels = ML_model_prediction(X_train, X_test, y_train, ML_model, **kwargs)
+    acc = y_test - labels
+    acc = 1 - np.dot(acc,acc) / len(y_test)
+    return acc
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,53 +8,65 @@ from numpy import genfromtxt
 from sklearn.model_selection import KFold
 
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC # Supprort Vector classifier
+from sklearn.neighbors import KNeighborsClassifier # KNN
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
 
 import sys
 sys.path.append('src')
 import models
 
-# ------------------- IMPORT DATA ----------------
-data = pd.read_csv (r'data/project_train.csv', encoding='utf-8') # Import data
-inputs = pd.DataFrame(data).to_numpy()
-print('size of data input')
-print(np.shape(inputs))
-np.random.shuffle(inputs)
-# -----------------  TRAINING DATA ---------------------------------
-X_train = inputs[0:374,0:11] # training data (without the labels)
-y_train = inputs[0:374,11] # training data (only labels)
-# ----------------- EVALUATION DATA ---------------------------------
-X_test = inputs[375:,0:11] # Using last 25% of data to evaluate how good the classifier is.
-y_val = inputs[375:,11] # correct labels on the evaluation data.
-print('-----')
+def main():
+    # ------------------- IMPORT DATA ----------------
+    data = pd.read_csv (r'data/project_train.csv', encoding='utf-8') # Import data
+    inputs = pd.DataFrame(data).to_numpy()
+    print('size of data input')
+    print(np.shape(inputs))
+    np.random.shuffle(inputs)
+    # -----------------  TRAINING DATA ---------------------------------
+    X_train = inputs[0:374,0:11] # training data (without the labels)
+    y_train = inputs[0:374,11] # training data (only labels)
+    # ----------------- EVALUATION DATA ---------------------------------
+    X_test = inputs[375:,0:11] # Using last 25% of data to evaluate how good the classifier is.
+    y_test = inputs[375:,11] # correct labels on the evaluation data.
+    print('-----')
 
+    acc = models.get_accuracy(
+        X_train, X_test, y_train, y_test,
+        SVC, C=1, kernel='linear'
+    )
+    print('Accuracy with SVM')
+    print(acc)
 
-labels = models.svm(X_train, X_test, y_train)
-print(labels)
-acc = y_val - labels
-acc = 1 - np.dot(acc,acc) / len(y_val)
-print('Accuracy with SVM')
-print(acc)
+    acc = models.get_accuracy(
+        X_train, X_test, y_train, y_test,
+        KNeighborsClassifier, n_neighbors=10, weights='distance', algorithm='auto'
+    )
+    print('Accuracy with KNN')
+    print(acc)
 
-labels_knn = models.knn(X_train, X_test, y_train)
-acc_knn = y_val - labels_knn
-acc_knn = 1 - np.dot(acc_knn,acc_knn) / len(y_val)
-print('Accuracy with KNN')
-print(acc_knn)
+    acc = models.get_accuracy(
+        X_train, X_test, y_train, y_test,
+        LinearDiscriminantAnalysis
+    )
+    print('Accuracy with LDA')
+    print(acc)
 
-labels_lda = models.lda(X_train, X_test, y_train)
-acc_lda = y_val - labels_lda
-acc_lda = 1 - np.dot(acc_lda,acc_lda) / len(y_val)
-print('Accuracy with LDA')
-print(acc_lda)
+    acc = models.get_accuracy(
+        X_train, X_test, y_train, y_test,
+        QuadraticDiscriminantAnalysis, reg_param=0.01
+    )
+    print('Accuracy with QDA')
+    print(acc)
 
-labels_qda = models.qda(X_train, X_test, y_train)
-acc_qda = y_val - labels_qda
-acc_qda = 1 - np.dot(acc_qda,acc_qda) / len(y_val)
-print('Accuracy with QDA')
-print(acc_qda)
+    acc = models.get_accuracy(
+        X_train, X_test, y_train, y_test,
+        RandomForestClassifier, criterion='entropy', n_estimators=200, max_features='sqrt'
+    )
+    print('Accuracy with rfc')
+    print(acc)
 
-labels_rfc = models.rfc(X_train, X_test, y_train)
-acc_rfc = y_val - labels_rfc
-acc_rfc = 1 - np.dot(acc_rfc,acc_rfc) / len(y_val)
-print('Accuracy with rfc')
-print(acc_rfc)
+if __name__ == '__main__':
+    main()
